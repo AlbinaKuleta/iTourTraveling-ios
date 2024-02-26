dc44478ff46f745b22b9859ace3ad79d39371f13
AlbinaKuleta <albina.kuleta@student.uni-pr.edu>
2024-02-26 14:32:52 -0800

    button added on views

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,19 +5,36 @@
 //  Created by Albina Kuleta on 26.2.24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+
+    
+    @State private var searchText = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+                
+                    Button("Add Destination", systemImage: "plus", action: addDestination)
+
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                      
+
+                            Text("Priority")
+                               
+
+                            Text("Date")
+                              
+                        }
+                        .pickerStyle(.inline)
+                    }
+                }
+    
+    func addDestination() {
+     
     }
-}
 
 #Preview {
     ContentView()
--- iTour/iTourApp.swift
+++ iTour/iTourApp.swift
@@ -5,6 +5,7 @@
 //  Created by Albina Kuleta on 26.2.24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iTourApp: App {
         WindowGroup {
             ContentView()
         }
+        
     }
 }
