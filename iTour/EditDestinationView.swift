a912bf5e1ef60eae28b5d3e673b6a034a4337544
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-02-27 14:33:29 -0800

    EditDestination file added

---
+++ iTour/EditDestinationView.swift
@@ -0,0 +1,8 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by ajshe selmani on 25.2.24.
+//
+
+import SwiftUI
---
+++ iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by ajshe selmani on 25.2.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
