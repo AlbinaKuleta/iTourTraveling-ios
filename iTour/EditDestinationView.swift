5d1036948d56dc2cc052f574d449c929941df184
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-02-28 11:46:47 -0800

    updates

--- iTour/EditDestinationView.swift
+++ iTour/EditDestinationView.swift
@@ -8,6 +8,41 @@
 import SwiftUI
 import SwiftData
 
+struct EditDestinationView: View {
+    @Bindable var destination: Destination
+    @State private var newSightName = ""
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $destination.name)
+            TextField("Details", text: $destination.details, axis: .vertical)
+            DatePicker("Date", selection: $destination.date)
+            
+            Section("Priority") {
+                Picker("Priority", selection: $destination.priority) {
+                    Text("Meh").tag(1)
+                    Text("Maybe").tag(2)
+                    Text("Must").tag(3)
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Sights") {
+                ForEach(destination.sights) { sight in
+                    Text(sight.name)
+                }
+                
+                HStack {
+                    TextField("Add a new sight in \(destination.name)", text: $newSightName)
+                    
+                    Button("Add", action: addSight)
+                }
+            }
+        }
+        .navigationTitle("Edit Destination")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
 
 
 #Preview {
