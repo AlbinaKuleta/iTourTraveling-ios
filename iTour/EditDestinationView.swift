7153befb717a0aac0345ebe13315e3982ac585b6
AlbinaKuleta <albina.kuleta@student.uni-pr.edu>
2024-02-29 00:55:58 -0800

    modification

--- iTour/EditDestinationView.swift
+++ iTour/EditDestinationView.swift
@@ -11,13 +11,13 @@ import SwiftData
 struct EditDestinationView: View {
     @Bindable var destination: Destination
     @State private var newSightName = ""
-    
+
     var body: some View {
         Form {
             TextField("Name", text: $destination.name)
             TextField("Details", text: $destination.details, axis: .vertical)
             DatePicker("Date", selection: $destination.date)
-            
+
             Section("Priority") {
                 Picker("Priority", selection: $destination.priority) {
                     Text("Meh").tag(1)
@@ -26,15 +26,15 @@ struct EditDestinationView: View {
                 }
                 .pickerStyle(.segmented)
             }
-            
+
             Section("Sights") {
                 ForEach(destination.sights) { sight in
                     Text(sight.name)
                 }
-                
+
                 HStack {
                     TextField("Add a new sight in \(destination.name)", text: $newSightName)
-                    
+
                     Button("Add", action: addSight)
                 }
             }
@@ -42,10 +42,10 @@ struct EditDestinationView: View {
         .navigationTitle("Edit Destination")
         .navigationBarTitleDisplayMode(.inline)
     }
-    
+
     func addSight() {
         guard newSightName.isEmpty == false else { return }
-        
+
         withAnimation {
             let sight = Sight(name: newSightName)
             destination.sights.append(sight)
@@ -54,13 +54,13 @@ struct EditDestinationView: View {
     }
 }
 
-
 #Preview {
     do {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Destination.self, configurations: config)
         let example = Destination(name: "Example Destination", details: "Example details go here and will automatically expand vertically as they are edited.")
-        
+        return EditDestinationView(destination: example)
+            .modelContainer(container)
     } catch {
         fatalError("Failed to create model container.")
     }
